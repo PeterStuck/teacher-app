d3004790676ec8e760a957fac71dc381db6eef99
Piotr Krawczyk <piotr.krawczyk@interia.pl>
2021-03-17 19:28:58 +0100

    add delete saved topic feature

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import MainNavigationView, LessonTopicsView, end_of_work_view
+from .views import MainNavigationView, LessonTopicsView, end_of_work_view, delete_saved_topic
 
 app_name = 'base'
 
 urlpatterns = [
     path('menu/', MainNavigationView.as_view(), name='main_nav'),
     path('saved-topics/', LessonTopicsView.as_view(), name='saved_topics'),
+    path('delete-topic/<int:pk>/', delete_saved_topic, name='delete_topic'),
     path('eow/', end_of_work_view, name='eow')
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
 from django.contrib.auth.decorators import login_required
 from dateutil.relativedelta import relativedelta
 from django.views.generic import TemplateView
@@ -34,10 +34,21 @@ class LessonTopicsView(LoginRequiredMixin, ListView):
     model = LessonTopic
     template_name = 'base/saved_teacher_topics.html'
     context_object_name = 'saved_topics'
-    paginate_by = 2
+    paginate_by = 20
 
     def get_queryset(self):
         return LessonTopic.objects.filter(teacher=self.request.user)
+
+
+@login_required(login_url='/login')
+def delete_saved_topic(request, pk):
+    try:
+        topic_to_delete = LessonTopic.objects.filter(teacher=request.user).get(pk=pk)
+        topic_to_delete.delete()
+
+        return HttpResponseRedirect(reverse('base:saved_topics'))
+    except LessonTopic.DoesNotExist:
+        return render(request, 'base/bad_topic_id.html', {})
 
 
 @login_required(login_url='/login')
